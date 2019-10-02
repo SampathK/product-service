2062a72a9c49b3df27d16bcfb4d92cd2efb0e577
ksampath <ksampath@anutanetworks.com>
2019-10-02 12:06:21 +0530

    update the db name

--- product-service/init-db.sh
+++ product-service/init-db.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker exec -i mysql mysql -u root --password=root123 userdb < src/main/resources/init-db.sql
+docker exec -i mysql mysql -u root --password=root123 productdb < src/main/resources/init-db.sql
